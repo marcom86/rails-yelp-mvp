c4df525ba6fce0770651867f6c3fbebb4f0ab5ec
Marco Montis <marcmontis7@gmail.com>
2020-03-18 16:21:16 +0100

    done

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -3,23 +3,26 @@ class RestaurantsController < ApplicationController
     @restaurants = Restaurant.all
   end
 
+  def show
+    @restaurant = Restaurant.find(params[:id])
+  end
+
   def new
     @restaurant = Restaurant.new
   end
 
   def create
     @restaurant = Restaurant.new(restaurant_params)
-    @restaurant.save
-    redirect_to restaurant_path(@restaurant)
-  end
-
-  def show
-    @restaurant = Restaurant.find(params[:id])
+    if @restaurant.save
+      redirect_to restaurant_path(@restaurant)
+    else
+      render "new"
+    end
   end
 
   private
 
   def restaurant_params
-    params.require(:restaurant).permit(:name, :address, :category)
+    params.require(:restaurant).permit(:name, :address, :phone_number, :category)
   end
 end
